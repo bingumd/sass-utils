97d637e2f2c795851524c66c0a83245a368bbdcc
bingumd <bingumd@gmail.com>
2021-11-13 19:04:44 +0200

    :arrow_up: upgrading dependencies

--- test.js
+++ test.js
@@ -1,43 +1,6 @@
-var fs = require('fs')
-const css = require('css')
-
-const file =  fs.readFileSync('./dist/utils.css', 'utf8')
-const parsed = css.parse(file);
-
-const other = []
-const declarations = []
-
-
-parsed.stylesheet.rules.forEach(current => {
-    const value = {}
-    // Ignore non-rule types
-
-    if (current.type !== 'rule') {
-        other.push(current)
-        return
-    }
-
-    const selector = current.selectors.join(',')
-
-    current.declarations.forEach(rule => {
-        value[rule.property] = rule.value
-    })
-
-    declarations.push(selector)
-})
-
-const findDuplicates = (arr) => {
-    let sorted_arr = arr.slice().sort();
-    let results = [];
-    for (let i = 0; i < sorted_arr.length - 1; i++) {
-        if (sorted_arr[i + 1] == sorted_arr[i]) {
-        results.push(sorted_arr[i]);
-      }
-    }
-    return results;
-}
+const getDuplicateCssSelectors = require('get-duplicate-css-selectors')
+const CSS = getDuplicateCssSelectors('./dist/utils.css')
 
 test('Find dublicate selector', () => {
-    const dublicate = findDuplicates(declarations)
-    expect(dublicate.toString()).toEqual('')
+    expect(CSS.toString()).toEqual('')
 })
